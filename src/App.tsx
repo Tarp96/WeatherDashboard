60fac9cca1032f9d7db19b9a49f3ce716d5216a8
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-04 16:17:31 +0200

    feat: App.tsx displays raw weather data using tanstack

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+import { getWeatherData } from "./services/api/WeatherService";
 
 function App() {
+  const { data, isPending } = useQuery({
+    queryKey: ["cityData"],
+    queryFn: () => getWeatherData("London"),
+  });
+
   return (
     <>
       <h1>Weather Dashboard</h1>
+      <p>{isPending ? <h2>Loading data....</h2> : JSON.stringify(data)}</p>
     </>
   );
 }
--- src/services/api/WeatherService.ts
+++ src/services/api/WeatherService.ts
@@ -1,5 +1,7 @@
-export const getWeatherData = async (city: String) => {
+export const getWeatherData = async (city: string) => {
     const response = 
     await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}`);
+    console.log(response.json())
     return response.json();
 }
+
