15daaa762ac6413d893a27dcfe6d808e5c6cf391
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-06 09:47:45 +0200

    refactor(weather-service): Change getWeatherData function to use metric measurement

--- src/services/api/WeatherService.ts
+++ src/services/api/WeatherService.ts
@@ -1,6 +1,6 @@
 export const getWeatherData = async (city: string) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
   );
 
   const data = await response.json();
