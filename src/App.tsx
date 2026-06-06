2cf1170ea747c930a7dd399cebbc485d8a768522
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-06 08:57:08 +0200

    refactor: Move Current weather display to seperate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import "./App.css";
-import { useQuery } from "@tanstack/react-query";
-import { getWeatherData } from "./services/api/WeatherService";
+import { CityWeatherDisplay } from "./components/CityWeatherDisplay";
 
 function App() {
-  const { data, isPending } = useQuery({
-    queryKey: ["cityData"],
-    queryFn: () => getWeatherData("London"),
-  });
-
-  if (isPending) return <h2>Loading data...</h2>;
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">Weather Dashboard</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <CityWeatherDisplay />
     </>
   );
 }
--- src/components/CityWeatherDisplay.tsx
+++ src/components/CityWeatherDisplay.tsx
@@ -1,1 +1,17 @@
-export const CityWeatherDisplay = () => {};
+import { useQuery } from "@tanstack/react-query";
+import { getWeatherData } from "../services/api/WeatherService";
+
+export const CityWeatherDisplay = () => {
+  const { data, isPending } = useQuery({
+    queryKey: ["cityData"],
+    queryFn: () => getWeatherData("Oslo"),
+  });
+
+  if (isPending) return <h2>Loading data...</h2>;
+
+  return (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
+  );
+};
