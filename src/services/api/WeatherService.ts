8de6661299ad5004a41ff91afd2e25caeffdd514
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-07 10:01:57 +0200

    feat(weather-service):
    - Add function to retrieve geo location data
    
    - Update getWeatherdata function to use geodata to display accurate weather with API 4.0

--- src/services/api/WeatherService.ts
+++ src/services/api/WeatherService.ts
@@ -1,12 +1,18 @@
-export const getWeatherData = async (city: string) => {
+export const getLongLat = async (city: string) => {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
-  );
-
+    `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${import.meta.env.VITE_API_KEY}`
+  )
   const data = await response.json();
-
-  console.log(data);
-
   return data;
-};
+}
+
+
+export const getWeatherData  = async (city: string) => {
+  const geoData = await getLongLat(city);
+  const response = await fetch(`
+    https://api.openweathermap.org/data/4.0/onecall/current?lat=${geoData.lat}&lon=${geoData.lon}&appid=${import.meta.env.VITE_API_KEY}
+    `)
+    const data = await response.json();
+    return data;
+}
 
