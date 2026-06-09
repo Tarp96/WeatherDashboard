a5a9a441b2ee3c25a435928574087df20d36c6f0
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-09 16:43:02 +0200

    refactor(home-page): Add titles for section seperation

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,12 @@ export const HomePage = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Weather Dashboard</h1>
-      <div></div>
+      <div>
+        <h2>Favorites</h2>
+      </div>
+      <div>
+        <h2>My Location</h2>
+      </div>
     </>
   );
 };
