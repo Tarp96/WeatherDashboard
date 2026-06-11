27fe928f93047bb20f9e519a09d339603a5b87af
Tarpi96 <tarpisingh.96@gmail.com>
2026-06-11 11:44:33 +0200

    feat(todays-temperature): Query forecast data and implement pending state

--- src/components/TodaysTemperature.tsx
+++ src/components/TodaysTemperature.tsx
@@ -1,4 +1,18 @@
+import { getFiveDayForecastData } from "../services/api/WeatherService";
+import { useQuery } from "@tanstack/react-query";
+import { FiveDayForecastResponse } from "../types/Weather";
+
 export const TodaysTemperature = () => {
+  const { data, isPending } = useQuery<FiveDayForecastResponse>({
+    queryKey: ["fiveDayForecastData"],
+    queryFn: () => getFiveDayForecastData("Oslo"),
+  });
+
+  if (isPending) return <h2>Data Loading</h2>;
+
+  const forecastData = data?.list;
+  console.log(forecastData);
+
   return (
     <>
       <h2>Today's Temperature</h2>
