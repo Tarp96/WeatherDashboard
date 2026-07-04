6e81625eafd38443ad4e1f1137b0974780a1b54d
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-04 13:20:29 +0200

    feat: Create description formatter function to avoid weather description wrapping in five day forecast cards

--- src/utils/helpers/HelperFunctions.ts
+++ src/utils/helpers/HelperFunctions.ts
@@ -2,3 +2,17 @@ export function firstLetterUpperCase(word: string) {
   if (!word) return "";
   return word[0].toUpperCase() + word.slice(1);
 }
+
+export function formatWeatherDescription(description: string){
+    const descriptions: Record<string, string> = {
+    "overcast clouds": "Overcast",
+    "broken clouds": "Broken clouds",
+    "scattered clouds": "Scattered",
+    "few clouds": "Few clouds",
+    "light intensity shower rain": "Light showers",
+    "moderate rain": "Rain",
+    "heavy intensity rain": "Heavy rain",
+  };
+
+  return descriptions[description.toLowerCase()] ?? firstLetterUpperCase(description);
+}
