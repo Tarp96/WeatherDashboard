0897d60930008cf53652b95520844361940aca6c
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-06 15:05:59 +0200

    style: Add search icon to searchbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import type { SubmitEventHandler } from "react";
+import { Search } from "lucide-react";
 
 type HeaderProps = {
   onSearch: (city: string) => void;
@@ -25,29 +26,32 @@ export const Header = ({ onSearch }: HeaderProps) => {
         </h1>
 
         <div className="relative">
-          <form onSubmit={handleSubmit}>
+          <form onSubmit={handleSubmit} className="relative">
+            <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+
             <input
               type="text"
               value={query}
               onChange={(e) => setQuery(e.target.value)}
               placeholder="Search city..."
               className="
-              w-72
-              rounded-xl
-              border
-              border-gray-300
-              bg-white
-              px-4
-              py-2
-              text-slate-700
-              placeholder:text-gray-400
-              shadow-sm
-              outline-none
-              transition
-              focus:border-blue-500
-              focus:ring-2
-              focus:ring-blue-200
-            "
+      w-72
+      rounded-xl
+      border
+      border-gray-300
+      bg-white
+      py-2
+      pl-10
+      pr-4
+      text-slate-700
+      placeholder:text-gray-400
+      shadow-sm
+      outline-none
+      transition
+      focus:border-blue-500
+      focus:ring-2
+      focus:ring-blue-200
+    "
             />
           </form>
         </div>
