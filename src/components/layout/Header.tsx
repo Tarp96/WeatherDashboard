80e2ff94436f481eb4430ab82830e808403d7829
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-08 21:44:58 +0200

    feat: Add button to searchbar

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -27,34 +27,62 @@ export const Header = ({ onSearch, isSearching }: HeaderProps) => {
         </h1>
 
         <div className="relative">
-          <form onSubmit={handleSubmit} className="relative">
-            <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
+          <form onSubmit={handleSubmit} className="flex items-center gap-3">
+            <div className="relative">
+              <Search className="absolute left-3 top-1/2 h-5 w-5 -translate-y-1/2 text-gray-400" />
 
-            <input
+              <input
+                disabled={isSearching}
+                type="text"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Search city..."
+                className="
+        w-72
+        rounded-xl
+        border
+        border-gray-300
+        bg-white
+        py-2
+        pl-10
+        pr-4
+        text-slate-700
+        placeholder:text-gray-400
+        shadow-sm
+        outline-none
+        transition
+        focus:border-blue-500
+        focus:ring-2
+        focus:ring-blue-200
+        disabled:cursor-not-allowed
+        disabled:opacity-60
+      "
+              />
+            </div>
+
+            <button
+              type="submit"
               disabled={isSearching}
-              type="text"
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Search city..."
               className="
-      w-72
-      rounded-xl
-      border
-      border-gray-300
-      bg-white
-      py-2
-      pl-10
-      pr-4
-      text-slate-700
-      placeholder:text-gray-400
-      shadow-sm
-      outline-none
-      transition
-      focus:border-blue-500
-      focus:ring-2
-      focus:ring-blue-200
-    "
-            />
+    flex items-center gap-2
+    cursor-pointer
+    rounded-xl
+    bg-blue-600
+    px-5
+    py-2
+    font-medium
+    text-white
+    transition
+    hover:bg-blue-700
+    focus:outline-none
+    focus:ring-2
+    focus:ring-blue-300
+    disabled:cursor-not-allowed
+    disabled:bg-blue-400
+  "
+            >
+              {isSearching ? "Searching..." : "Search"}
+            </button>
           </form>
         </div>
       </div>
