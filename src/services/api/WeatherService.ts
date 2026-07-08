8f97792e9049ac130f3a5375b983635f9c0e544b
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-08 12:34:25 +0200

    feat(service): Create reverse geocoding function

--- src/services/api/WeatherService.ts
+++ src/services/api/WeatherService.ts
@@ -1,8 +1,8 @@
 import { CityNotFoundError } from "../../components/errors/CityNotFoundError";
 
 export const getWeatherData = async (lat: number, lon: number) => {
-   const response = await fetch(
-    `https://api.openweathermap.org/data/4.0/onecall/current?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
+  const response = await fetch(
+    `https://api.openweathermap.org/data/4.0/onecall/current?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`,
   );
 
   if (!response.ok) {
@@ -10,17 +10,28 @@ export const getWeatherData = async (lat: number, lon: number) => {
   }
 
   return response.json();
-}
+};
 
 export const getCoordinatesByCity = async (city: string) => {
   const response = await fetch(
-    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${import.meta.env.VITE_API_KEY}`
+    `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${import.meta.env.VITE_API_KEY}`,
   );
 
   if (!response.ok) {
     throw new Error("Failed to fetch coordinates");
   }
 
+  return response.json();
+};
+
+export const getCityWithCoordinates = async (lat: number, lon: number) => {
+  const response = await fetch(`
+    http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit={limit}&appid=${import.meta.env.VITE_API_KEY}
+    `);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch city name");
+  }
   return response.json();
 };
 
@@ -43,24 +54,22 @@ export const getWeatherDataWithCoordinates = async (city: string) => {
   };
 };
 
-
 export const getFiveDayForecastData = async (city: string) => {
-  const geoData = await getCoordinatesByCity(city)
+  const geoData = await getCoordinatesByCity(city);
 
-   if (!geoData.length) {
-  throw new CityNotFoundError(city);
-}
+  if (!geoData.length) {
+    throw new CityNotFoundError(city);
+  }
 
-  const {lat, lon} = geoData[0]
+  const { lat, lon } = geoData[0];
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`
-  )
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${import.meta.env.VITE_API_KEY}&units=metric`,
+  );
 
-    if (!response.ok) {
+  if (!response.ok) {
     throw new Error("Failed to fetch weather data");
   }
 
   const forecastData = await response.json();
   return forecastData;
 };
-
