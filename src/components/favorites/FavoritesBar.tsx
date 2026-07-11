856283aa0c99b0728a892996ac2465a1b41f5b88
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-11 12:37:56 +0200

    feat: Implement FavoritesBar component to display favorite cities and their weather data

--- src/components/favorites/FavoritesBar.tsx
+++ src/components/favorites/FavoritesBar.tsx
@@ -1,1 +1,17 @@
-export const FavoritesBars = () => {};
+import { getFavoriteCities } from "../../utils/helpers/Favorites";
+import { useQueries } from "@tanstack/react-query";
+import { getWeatherDataWithCoordinates } from "../../services/api/WeatherService";
+
+export const FavoritesBars = () => {
+  const favoriteCityList = getFavoriteCities();
+
+  if (favoriteCityList.length <= 0 || favoriteCityList === null)
+    return <h2>No favorites added yet</h2>;
+
+  const favoriteQueries = useQueries({
+    queries: favoriteCityList.map((city) => ({
+      queryKey: ["favorite"],
+      queryFn: () => getWeatherDataWithCoordinates(city),
+    })),
+  });
+};
