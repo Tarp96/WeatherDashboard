a4d1e9a28da0092ac5b874fea1a4cf00bdae3bed
Tarpi96 <tarpisingh.96@gmail.com>
2026-07-12 18:45:45 +0200

    feat: Add air pollution data interfaces for API response handling

--- src/types/Weather.ts
+++ src/types/Weather.ts
@@ -87,3 +87,29 @@ export interface ForecastRain {
 export interface ForecastCity {
   timezone: number;
 }
+
+export interface AirPollutionResponse {
+  coord: [number, number];
+  list: AirPollutionData[];
+}
+
+export interface AirPollutionData {
+  dt: number;
+  main: AirQualityIndex;
+  components: AirPollutionComponents;
+}
+
+export interface AirQualityIndex {
+  aqi: 1 | 2 | 3 | 4 | 5;
+}
+
+export interface AirPollutionComponents {
+  co: number;
+  no: number;
+  no2: number;
+  o3: number;
+  so2: number;
+  pm2_5: number;
+  pm10: number;
+  nh3: number;
+}
